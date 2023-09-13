57ffb58000f2f26d7be91c9de82c970146d86a3a
Olya <olyamoruga@yandex.ru>
2023-09-13 18:02:52 +0300

    added an example programm for  condition if else

---
+++ Example005_Condition_ifelse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
